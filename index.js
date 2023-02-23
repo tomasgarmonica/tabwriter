25b8e5245f8cfb286d99c4dc53b423465aca26a2
tomasgarmonica <gonzaleztomase@gmail.com>
2023-02-23 02:16:52 -0300

    Agregada función teclado, referencias y títulos

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+const armonica = document.getElementById("armo-el")
 let tabEl = document.getElementById("tab-el");
 let aeBtn = document.getElementById("ae-btn");
 let uaBtn = document.getElementById("ua-btn");
 let auBtn = document.getElementById("au-btn");
 let uauBtn = document.getElementById("uau-btn");
-let tBtn = document.getElementById("t-btn")
+let tBtn = document.getElementById("t-btn");
 let aeState = true;
 let tState = true;
 
@@ -63,6 +64,140 @@ function copiarAPortaPapeles(){
 
     alert("Tab copiada al portapapeles");
 }
+
+
+
+//Modo teclado
+window.addEventListener('keydown', (event) => {
+    event.preventDefault();
+    switch(event.code){
+        case "Enter": 
+            saltoDeLinea();
+            break;
+        case "Backspace": 
+            borrar();
+            break;
+        case "Minus": 
+            celdaPulsada('10bb');
+            break;
+        case "Digit1": 
+            celdaPulsada('1ob');
+            break;
+        case "Digit4": 
+            celdaPulsada('4ob');
+            break;
+        case "Digit5": 
+            celdaPulsada('5ob');
+            break;
+        case "Digit6": 
+            celdaPulsada('6ob');
+            break;
+        case "Digit8": 
+            celdaPulsada('8b');
+            break;
+        case "Digit9": 
+            celdaPulsada('9b');
+            break;
+        case "Digit0": 
+            celdaPulsada('10b');
+            break;
+        case "KeyQ": 
+            celdaPulsada('1');
+            break;
+        case "KeyW": 
+            celdaPulsada('2');
+            break;
+        case "KeyE": 
+            celdaPulsada('3');
+            break;
+        case "KeyR": 
+            celdaPulsada('4');
+            break;
+        case "KeyT": 
+            celdaPulsada('5');
+            break;
+        case "KeyY": 
+            celdaPulsada('6');
+            break;
+        case "KeyU": 
+            celdaPulsada('7');
+            break;
+        case "KeyI": 
+            celdaPulsada('8');
+            break;
+        case "KeyO": 
+            celdaPulsada('9');
+            break;
+        case "KeyP": 
+            celdaPulsada('10');
+            break;
+        case "KeyA": 
+            celdaPulsada('(1)');
+            break;
+        case "KeyS": 
+            celdaPulsada('(2)');
+            break;
+        case "KeyD": 
+            celdaPulsada('(3)');
+            break;
+        case "KeyF": 
+            celdaPulsada('(4)');
+            break;
+        case "KeyG": 
+            celdaPulsada('(5)');
+            break;
+        case "KeyH": 
+            celdaPulsada('(6)');
+            break;
+        case "KeyJ": 
+            celdaPulsada('(7)');
+            break;
+        case "KeyK": 
+            celdaPulsada('(8)');
+            break;
+        case "KeyL": 
+            celdaPulsada('(9)');
+            break;
+        case "Semicolon": 
+            celdaPulsada('(10)');
+            break;
+        case "IntlBackslash": 
+            celdaPulsada('(1)b');
+            break;
+        case "KeyZ": 
+            celdaPulsada('(2)b');
+            break;
+        case "KeyC": 
+            celdaPulsada('(3)b');
+            break;
+        case "KeyN": 
+            celdaPulsada('(4)b');
+            break;
+        case "KeyM": 
+            celdaPulsada('(6)b');
+            break;
+        case "Comma": 
+            celdaPulsada('(7)od');
+            break;
+        case "Period": 
+            celdaPulsada('(9)od');
+            break;
+        case "Slash": 
+            celdaPulsada('(10)od');
+            break;
+        case "KeyX": 
+            celdaPulsada('(2)bb');
+            break;
+        case "KeyV": 
+            celdaPulsada('(3)bb');
+            break;
+        case "KeyB": 
+            celdaPulsada('(3)bbb');
+            break;
+    }
+});
+
+
 
 
 
